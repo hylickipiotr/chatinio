d789ceefcfbb08b3a851de37732664de89b81ae0
nexus0212 <pietrek0212@gmail.com>
2020-01-19 22:43:40 +0100

    :construction: Created basic requirements

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,67 @@
-/* eslint-env broweser */
+/* eslint-env browser */
 /* global io */
 const socket = io();
+
+const localState = {
+  user: {
+    name: '',
+  },
+};
+
+const usersListElement = document.querySelector('#chat-users');
+const messagesListElement = document.querySelector('#chat-messages');
+const newMessageFormElement = document.querySelector('#message-form');
+const newMessageTextElement = newMessageFormElement.querySelector('.__content');
+
+
+function emitMessage(message) {
+  socket.emit('chat-message', message);
+}
+
+function setUser(user) {
+  socket.emit('set-user', user);
+}
+
+function renderUsers(users) {
+  usersListElement.innerHTML = '';
+
+  Object.values(users).forEach((user) => {
+    const userElement = document.createElement('li');
+    userElement.className = 'user';
+    userElement.textContent = user.name;
+
+    usersListElement.appendChild(userElement);
+  });
+}
+
+function renderMessage(message) {
+  const messageElement = document.createElement('div');
+  const date = new Date(message.timestamp).toLocaleString();
+  messageElement.innerHTML = `
+    <h5 class="username">${message.user.name}</h5>
+    <p class="content">${message.content}</p>
+    <small class="date">${date}</small>
+  `;
+  messagesListElement.appendChild(messageElement);
+}
+
+newMessageFormElement.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const message = newMessageTextElement.value;
+  if (!message.length) return;
+  emitMessage(message);
+});
+
+socket.on('connect', () => {
+  const { user } = localState;
+  if (!user.name) {
+    while (!user.name) {
+      user.name = prompt('Enter user name').trim();
+    }
+  }
+  setUser(user);
+});
+
+socket.on('chat-users', (users) => renderUsers(users));
+
+socket.on('chat-message', (message) => renderMessage(message));
--- src/sockets.js
+++ src/sockets.js
@@ -1,9 +1,60 @@
 const socketIO = require('socket.io');
 
+const serverState = {
+  users: {},
+  activeConnections: 0,
+};
+
 function init(server) {
   const io = socketIO(server);
-  io.on('connection', () => {
-    console.log('a user connected');
+  io.on('connection', (socket) => {
+    serverState.activeConnections++;
+    console.log(`User connected. Number of active connections: ${serverState.activeConnections}`);
+
+    function getUser() {
+      return serverState.users[socket.id];
+    }
+
+    function emitUsers() {
+      io.emit('chat-users', serverState.users);
+    }
+
+    function emitMessage(message) {
+      io.emit('chat-message', message);
+    }
+
+    socket.on('set-user', (user) => {
+      serverState.users[socket.id] = {
+        name: user.name,
+      };
+      emitUsers();
+      console.log(`User '${getUser().name}' joined to chat`);
+    });
+
+
+    socket.on('chat-message', (msg) => {
+      const message = {
+        user: getUser(),
+        timestamp: Date.now(),
+        content: msg,
+      };
+
+      emitMessage(message);
+    });
+
+    socket.on('disconnect', () => {
+      const user = getUser();
+      serverState.activeConnections--;
+      if (!user) {
+        console.log(`User disconnected. Number of active connections: ${serverState.activeConnections}`);
+        return;
+      }
+      if (user.name) {
+        delete serverState.users[socket.id];
+      }
+      console.log(`User '${user.name}' disconnected. Number of active connections: ${serverState.activeConnections}`);
+      emitUsers();
+    });
   });
 }
 
